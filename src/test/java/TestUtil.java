911e0e656784abd5d0b7794ddfa0e9531859909c
ujineeee <yujin11006@naver.com>
2023-03-06 15:40:05 +0900

    종료기능 구현, 올바르지 않은 명령어 처리

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -1,5 +1,11 @@
+import com.ll.App;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import java.io.*;
 import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestUtil {
     // gen == generate 생성하다.
@@ -23,4 +29,43 @@ public class TestUtil {
             throw new RuntimeException(e);
         }
     }
+    // 테스트유틸 테스트 끝
+
+    // 앱 테스트 시작
+    @Test
+    @DisplayName("프로그램 시작시 타이틀 출력 그리고 종료")
+    public void t3() {
+        Scanner sc = TestUtil.genScanner("종료");
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs)
+                .contains("== 명언 앱 ==")
+                .contains("명령) ")
+                .contains("프로그램이 종료되었습니다.")
+                .doesNotContain("올바르지 않은 명령입니다.");
+    }
+
+    @Test
+    @DisplayName("잘못된 명령어 입력에 대한 처리")
+    public void t4() {
+        Scanner sc = TestUtil.genScanner("""
+                종료2
+                종료
+                """.stripIndent().trim());
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs)
+                .contains("올바르지 않은 명령입니다.");
+    }
+    // 앱 테스트 끝
 }
